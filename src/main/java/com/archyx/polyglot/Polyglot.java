ffc92012443a682a0a2df191814d09dad7337f02
Archy-X <archydevelopment@gmail.com>
2022-10-22 18:36:37 -0400

    Create instance of MessageManager in Polyglot

--- src/main/java/com/archyx/polyglot/Polyglot.java
+++ src/main/java/com/archyx/polyglot/Polyglot.java
@@ -1,16 +1,19 @@
 package com.archyx.polyglot;
 
 import com.archyx.polyglot.config.PolyglotConfig;
+import com.archyx.polyglot.lang.MessageManager;
 import org.bukkit.plugin.Plugin;
 
 public class Polyglot {
 
     private final Plugin plugin;
     private final PolyglotConfig config;
+    private final MessageManager messageManager;
 
     public Polyglot(Plugin plugin, PolyglotConfig config) {
         this.plugin = plugin;
         this.config = config;
+        this.messageManager = new MessageManager(this);
     }
 
     public Plugin getPlugin() {
@@ -21,4 +24,7 @@ public class Polyglot {
         return config;
     }
 
+    public MessageManager getMessageManager() {
+        return messageManager;
+    }
 }
