196fe7ffc1e580d2866bad97313c74fd8bea23ea
Archy-X <63976867+Archy-X@users.noreply.github.com>
2024-06-14 17:37:11 -0700

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,17 +8,14 @@ repositories {
 }
 
 dependencies {
-    compileOnly("net.kyori:adventure-text-minimessage:4.14.0")
-    compileOnly("net.kyori:adventure-api:4.14.0")
-    compileOnly("net.kyori:adventure-text-serializer-legacy:4.14.0")
-    compileOnly("org.spongepowered:configurate-yaml:4.1.2")
+    implementation("org.spongepowered:configurate-yaml:4.1.2")
     compileOnly("org.jetbrains:annotations:24.1.0")
     testImplementation("org.junit.jupiter:junit-jupiter")
-    testImplementation(platform("org.junit:junit-bom:5.10.1"))
+    testImplementation(platform("org.junit:junit-bom:5.10.2"))
 }
 
 group = "com.archyx"
-version = "1.1.1"
+version = "1.2.0"
 description = "Polyglot"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
